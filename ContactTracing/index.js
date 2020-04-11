db2929f89be2612c54d34845bfe36e0742f1099c
Richard Lindhout <richardlindhout96@gmail.com>
2020-04-11 23:23:09 +0200

    Working out privacy first Corona tracking app

---
+++ ContactTracing/index.js
@@ -0,0 +1,17 @@
+/**
+ * @format
+ */
+
+import React from 'react';
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+import {Provider as PaperProvider} from 'react-native-paper';
+export default function Main() {
+  return (
+    <PaperProvider>
+      <App />
+    </PaperProvider>
+  );
+}
+AppRegistry.registerComponent(appName, () => Main);
