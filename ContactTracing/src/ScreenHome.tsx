a9564eda629a102def6e4313b9d11b00815a318b
Richard Lindhout <richardlindhout96@gmail.com>
2020-04-13 20:14:20 +0200

    Added proof-of-concept iOS tracking

--- ContactTracing/src/ScreenHome.tsx
+++ ContactTracing/src/ScreenHome.tsx
@@ -9,8 +9,6 @@ import {
   PERMISSIONS,
   Permission,
 } from 'react-native-permissions'
-import { syncRSSI } from './DatabaseUtils'
-import { RSSIMap } from './types'
 
 async function requestBluetoothStatus() {
   const permission: Permission = Platform.select({
@@ -27,77 +25,11 @@ async function requestBluetoothStatus() {
 
 function App() {
   const [isTracking, setIsTracking] = useState(false)
-  const [infectionModalOpen, setInfectionModalOpen] = useState(false)
-
-  const manager = React.useRef<BleManager>()
-  const rssiPerHash = React.useRef<RSSIMap>({})
-
-  // Debounce callback
-  const [syncValues] = useDebouncedCallback(
-    // function
-    async () => {
-      const hasSynced = await syncRSSI(rssiPerHash.current)
-      if (hasSynced) {
-        rssiPerHash.current = {}
-      }
-    },
-    // delay in ms
-    10000 // sync after 10 seconds
-  )
 
   const startTracing = () => {
     async function startProcess() {
       const bluetoothStatus = await requestBluetoothStatus()
-      if (bluetoothStatus === 'denied') {
-        alert('Geen toestemming tot Bluetooth')
-        return
-      }
-      if (!manager.current) {
-        manager.current = new BleManager({
-          // restoreStateIdentifier
-          // restoreStateFunction
-        })
-      }
-      setIsTracking(true)
-      manager.current.startDeviceScan(
-        null, // uuids
-        {}, // options
-        (error, scannedDevice) => {
-          console.log({
-            error,
-            scannedDevice,
-          })
-          if (error) {
-            return
-          }
-          if (!scannedDevice) {
-            return
-          }
-          if (!scannedDevice.rssi) {
-            return
-          }
-
-          // we map rssi values so we can see if device did come closer
-          // Als gemeten in negatieve getallen betekent een getal dat dichter bij 0 ligt meestal een beter signaal,
-          // een getal dat -50 (min 50) is een redelijk goed signaal,
-          // een getal van -70 (min 70) is redelijk terwijl een getal dat -100 (min 100) is helemaal geen signaal heeft.
-          if (scannedDevice.rssi > 70) {
-            // not relevant enough
-            return
-          }
-          // https://iotandelectronics.wordpress.com/2016/10/07/how-to-calculate-distance-from-the-rssi-value-of-the-ble-beacon/
-          const previousRSSI = rssiPerHash.current[scannedDevice.id]
-          const latestRSSI = scannedDevice.rssi
-
-          rssiPerHash.current[scannedDevice.id] = previousRSSI
-            ? latestRSSI > previousRSSI
-              ? latestRSSI
-              : previousRSSI
-            : latestRSSI
-
-          syncValues()
-        }
-      )
+      // TODO: start background status
     }
     startProcess()
   }
--- ContactTracing/src/Tracing.tsx
+++ ContactTracing/src/Tracing.tsx
@@ -1,18 +1,98 @@
 import { Platform } from 'react-native'
+import { BleManager } from 'react-native-ble-plx'
+import TrackPlayer,{Track} from 'react-native-track-player';
 import BackgroundService from 'react-native-background-actions'
+import { RSSIMap } from './types'
+
+const track:Track = {
+  id: 'trackId',
+  url: require('../assets/5-minutes-of-silence.mp3'),
+  title: 'Corona Tracing',
+  artist: 'Track Artist',
+  artwork: require('track.png')
+}
+
+// 1 year of music
+let arrayOfTracks:Track[] = []
+var i;
+for (i = 0; i < 105120; i++) {
+  arrayOfTracks.push(track)
+}
+
+
+// this is used keep the bluetooth working in background
+function play1YearOfSummer() {
+  TrackPlayer.setupPlayer().then(async () => {
+
+    // Adds a track to the queue
+    await TrackPlayer.add(arrayOfTracks);
+
+    // Starts playing it
+    TrackPlayer.play();
+
+});
+}
+
 // You can do anything in your task such as network requests, timers and so on,
 // as long as it doesn't touch UI. Once your task completes (i.e. the promise is resolved),
 // React Native will go into "paused" mode (unless there are other tasks running,
 // or there is a foreground app).
-const scanForBluetoothDevices = async (taskDataArguments) => {
+const scanForBluetoothDevices = async () => {
+
+  const rssiPerHash: RSSIMap = {}
+
   // Example of an infinite loop task
-  const { delay } = taskDataArguments
+  
   await new Promise((resolve) => {
     for (let i = 0; BackgroundService.isRunning(); i++) {
-      console.log(i)
-      await sleep(delay)
-    }
-  })
+
+
+      // ;-)
+      if (Platform.OS === 'ios') {
+        play1YearOfSummer()
+      }
+
+   
+      const manager = new BleManager({
+        // restoreStateIdentifier
+        // restoreStateFunction
+      })
+      
+      manager.startDeviceScan(
+        null, // uuids
+        {}, // options
+        (error, scannedDevice) => {
+          console.log({
+            error,
+            scannedDevice,
+          })
+
+          if (error) {
+            return
+          }
+
+          // not relevant 
+          if (!scannedDevice || !scannedDevice.rssi || scannedDevice.rssi > 70) {
+            return
+          }
+
+          // we map rssi values so we can see if device did come closer
+          // Als gemeten in negatieve getallen betekent een getal dat dichter bij 0 ligt meestal een beter signaal,
+          // een getal dat -50 (min 50) is een redelijk goed signaal,
+          // een getal van -70 (min 70) is redelijk terwijl een getal dat -100 (min 100) is helemaal geen signaal heeft.
+          
+          // https://iotandelectronics.wordpress.com/2016/10/07/how-to-calculate-distance-from-the-rssi-value-of-the-ble-beacon/
+          const previousRSSI = rssiPerHash[scannedDevice.id]
+          const latestRSSI = scannedDevice.rssi
+
+          rssiPerHash[scannedDevice.id] = previousRSSI
+            ? latestRSSI > previousRSSI
+              ? latestRSSI
+              : previousRSSI
+            : latestRSSI
+     
+    })
+
 }
 
 const options = {
