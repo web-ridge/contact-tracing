17c2a5e87570f85818bb615122e6ecdf39cd5024
Richard Lindhout <richardlindhout96@gmail.com>
2020-04-16 00:09:25 +0200

    Finish basic risk evaluation

--- backend/resolver.go
+++ backend/resolver.go
@@ -5,8 +5,10 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"time"
 
 	"github.com/rs/zerolog/log"
+	"github.com/volatiletech/null"
 	fm "github.com/web-ridge/contact-tracing/backend/graphql_models"
 	. "github.com/web-ridge/contact-tracing/backend/helpers"
 	dm "github.com/web-ridge/contact-tracing/backend/models"
@@ -29,7 +31,7 @@ func (r *mutationResolver) CreateInfectedEncounters(ctx context.Context, input f
 
 // groupInfectedEncounterOnFirstPartOfHash so we know how many interactions with this hash
 // not the full hash because we don't want it to be tracable
-func groupInfectedEncounterOnFirstPartOfHash(infectedEncounters []*dm.InfectedEncounter) map[string]*dm.InfectedEncounter {
+func groupInfectedEncounterOnFirstPartOfHash(infectedEncounters []*dm.InfectedEncounter) map[string][]*dm.InfectedEncounter {
 	m := make(map[string][]*dm.InfectedEncounter)
 	for _, infectedEncounter := range infectedEncounters {
 		key := infectedEncounter.StartOfCreatorHash.String
@@ -43,18 +45,15 @@ func groupInfectedEncounterOnFirstPartOfHash(infectedEncounters []*dm.InfectedEn
 	return m
 }
 
-
-
 // getRiskPercentage returns int based on encounters with maximum risk
-func getRisk(infectedEncounters []*dm.InfectedEncounter) (int) {
+func getRisk(infectedEncounters []*dm.InfectedEncounter) int {
 
 	risk := 0
 
-	
 	for _, encounter := range infectedEncounters {
 		if !encounter.Rssi.Valid {
 			continue
-		} 
+		}
 
 		// not so many hits probably not good enough
 		if encounter.Hits.Int < 5 {
@@ -63,7 +62,7 @@ func getRisk(infectedEncounters []*dm.InfectedEncounter) (int) {
 
 		if encounter.Rssi.Int > -65 {
 			risk += 4
-		} else if  encounter.Rssi.Int > -70 {
+		} else if encounter.Rssi.Int > -70 {
 			if encounter.Hits.Int > 50 {
 				risk += 2
 			} else {
@@ -76,36 +75,41 @@ func getRisk(infectedEncounters []*dm.InfectedEncounter) (int) {
 }
 
 func (r *queryResolver) InfectedEncounters(ctx context.Context, hash string) (*fm.InfectionSummary, error) {
-	// TODO get encounters for hash
-	// time.now()
-	// time.now - incubation period
-	// make infection summary
 
+	//  1-14 days, most commonly around five days.
+	now := time.Now()
+	beginOfIncubationPeriod := now.AddDate(0, 0, -14)
+
+	// get infected encounters for this hash in incubation period
 	infectedEncounters, err := dm.InfectedEncounters(
-	// TODO check with time and incubation period
-	// dm.InfectedEncounterWhere.Time.LTE()
-	// dm.InfectedEncounterWhere.Time.GTE()
+		dm.InfectedEncounterWhere.PossibleInfectedHash.EQ(null.StringFrom(hash)),
+		dm.InfectedEncounterWhere.Time.LTE(null.NewTime(now, true)),
+		dm.InfectedEncounterWhere.Time.GTE(null.NewTime(beginOfIncubationPeriod, true)),
 	).All(ctx, r.db)
+
 	if err != nil {
-		log.Error().Err(err).Msg("could not open database connection")
+		log.Error().Err(err).Msg("could not get infected encounters from database")
 		return nil, fmt.Errorf("could not get summary from database")
 	}
 
+	groupedEncountersPerHash := groupInfectedEncounterOnFirstPartOfHash(infectedEncounters)
 
-
-	groupedEncounters := groupInfectedEncounterOnFirstPartOfHash(infectedEncounters)
-	
 	highRiskInteractions := 0
 	middleRiskInteractions := 0
 	lowRiskInteractions := 0
 
-	for _, groupedEncounters := groupedEncountersPerHash {
-		getRisk(groupedEncounters)
-		// TODO, TODO
+	for _, value := range groupedEncountersPerHash {
+		risk := getRisk(value)
+		if risk > 10 {
+			highRiskInteractions++
+		} else if risk > 5 {
+			middleRiskInteractions++
+		} else {
+			lowRiskInteractions++
+		}
 	}
 
 	// GroupBy first part of hash
-
 	return &fm.InfectionSummary{
 		HighRiskInteractions:   highRiskInteractions,
 		MiddleRiskInteractions: middleRiskInteractions,
