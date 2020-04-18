c2dc4c97a83cf5e8052ea90dd9db5670b631f35f
Richard Lindhout <richardlindhout96@gmail.com>
2020-04-18 03:49:21 +0200

    Refactor more secure and anonymous

---
+++ node_modules/react-native-ito-bluetooth/index.js
@@ -0,0 +1,5 @@
+import { NativeModules } from 'react-native';
+
+const { ItoBluetooth } = NativeModules;
+
+export default ItoBluetooth;
---
+++ node_modules/react-native-ito-bluetooth/ios/ItoBluetooth.h
@@ -0,0 +1,5 @@
+#import <React/RCTBridgeModule.h>
+
+@interface ItoBluetooth : NSObject <RCTBridgeModule>
+
+@end
