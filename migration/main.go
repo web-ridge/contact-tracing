26782c1f3de94657628c8e79853dfe01ceeb66ce
Richard Lindhout <richardlindhout96@gmail.com>
2020-04-18 19:25:32 +0200

    Devicekey backend and more information about it

--- migration/main.go
+++ migration/main.go
@@ -11,22 +11,22 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-// Use GORM to create and migrate tables easilys
+//InfectedEncounter contains encounters which have been marked as a possible COVID-19 encounter
 type InfectedEncounter struct {
-	ID                   uint   `gorm:"primary_key"`
+	ID                   uint   `gorm:"primary_key"`         // auto increment
 	RandomPart           string `gorm:"not null"`            // Part of hash of infected person so we group them together as one
 	PossibleInfectedHash string `gorm:"not null;index:hash"` // Encountered Hash + index since we search on this a lot
 	RSSI                 int    `gorm:"not null"`            // how strong the signal was
 	Hits                 int    `gorm:"not null"`            // how many times this signal hit
-	Time                 int    `gorm:"not null"`
+	Time                 int    `gorm:"not null"`            // Date of contact (no hours and seconds sent in app)
 }
 
-// generated in the device and used to fetch risk information
+// DeviceKey generated in the device and used to fetch risk information
 type DeviceKey struct {
-	ID       uint   `gorm:"primary_key"`
-	Key      string `gorm:"not null;index:key"`
-	Password string `gorm:"not null;index:password"`
-	Time     int    `gorm:"not null"`
+	ID       uint   `gorm:"primary_key"`             // auto increment
+	Key      string `gorm:"not null;index:key"`      // This key is publicely available for other devices (Bluetooth Service UUID)
+	Password string `gorm:"not null;index:password"` // Generated at device and used to fetch alerts secure so no one else can see the alerts on their deviceKey
+	Time     int    `gorm:"not null"`                // We want to remove after 2 weeks
 }
 
 func main() {
